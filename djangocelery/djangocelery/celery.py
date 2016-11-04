a19f31ddd47a1d897ceff54c3561a4b8015d7e0c
Aleksandr <sashpro@mail.ru>
2016-11-04 09:38:20 +0200

    setup celery with redis

---
+++ djangocelery/djangocelery/celery.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocelery.settings')
+
+from django.conf import settings  # noqa
+
+app = Celery('')
+
+app = Celery('djangocelery',
+             broker='amqp://',
+             #backend='amqp://',
+             include=['djangocelery.tasks'])
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+)
+
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+if __name__ == '__main__':
+    app.start()
+
